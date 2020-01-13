61985499a74bc395920734f7490ce0a5bbb939a7
SirPotatoIV <otoolej33@gmail.com>
2020-01-13 17:13:21 -0600

    Vote is now saved to firestore.

--- public/js/index.js
+++ public/js/index.js
@@ -13,12 +13,20 @@ function main(){
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
     firebase.analytics();
+
+    const firestore = firebase.firestore()
+    const docRef = firestore.doc("samples/petData")
     
-    // get document elements
+    // get login document elements
     const emailInputEl = document.getElementById("emailInput");
     const passwordInputEl = document.getElementById("passwordInput");
     const loginBtnEl = document.getElementById("loginBtn");
     const loginStatusEl = document.getElementById("loginStatus");
+
+    // get voting document elements
+    const petChoiceEls = document.getElementsByName("pet");
+    const voteBtnEl = document.getElementById("voteBtn");
+    const petDislayEl = document.getElementById("petDisplay");
 
     function login(){
         // add login event
@@ -43,14 +51,55 @@ function main(){
         // checks if user is logged in or not
         firebase.auth().onAuthStateChanged(firebaseUser => {
             if(firebaseUser){
-                console.log(firebaseUser)
+                // console.log(firebaseUser)
                 loginStatusEl.innerText = "You are logged in."
             } else {
-                console.log("not logged in")
+                // console.log("not logged in")
                 loginStatusEl.innerText = "You are NOT logged in."               
             }
         })
     }
     authenticationState()
+
+    function voteBtn(){
+        voteBtnEl.addEventListener("click", function(){
+            const vote = getVote();
+            saveToDatabase(vote);
+        })
+    }
+    voteBtn()
+
+    function getVote(){
+        for(let i=0; i < petChoiceEls.length; i++){
+            if(petChoiceEls[i].checked){
+                return petChoiceEls[i].value;
+            }
+        }
+    }
+
+    async function saveToDatabase(vote){
+        const textToSave = vote;
+        console.log(`I am going to save ${vote} as the best pet.`)
+        try{ 
+            const response = await docRef.set({
+                petStatus: textToSave
+            })
+            console.log("Pet vote saved!: ", response)
+        }
+        catch(err){
+            console.log("Error occurred trying to save vote: ", err)
+        } 
+    }
+
+    function getRealTimeUpdates(){
+        docRef.onSnapshot(function(doc){
+            if(doc && doc.exists) {
+                const myData = doc.data();
+                petDislayEl.innerHTML = myData.petStatus;
+            }
+        })
+    }
+    getRealTimeUpdates()
+
 }
 main();
