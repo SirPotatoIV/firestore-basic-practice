8072646ea5500a9f8e45cacf75609dc7679d4213
SirPotatoIV <otoolej33@gmail.com>
2020-01-13 17:33:28 -0600

    Updated comments to better explain code. Firestore update listener only starts if a user is logged in.

--- js/index.js
+++ js/index.js
@@ -38,7 +38,7 @@ function main(){
             const auth = firebase.auth()
             // sign in
             try{
-                const response = await auth.signInWithEmailAndPassword(email, password);
+                await auth.signInWithEmailAndPassword(email, password);
             }
             catch(err){
                 console.log("issue occurred during login: ", err)
@@ -51,10 +51,12 @@ function main(){
         // checks if user is logged in or not
         firebase.auth().onAuthStateChanged(firebaseUser => {
             if(firebaseUser){
-                // console.log(firebaseUser)
+                // User is not logged in
                 loginStatusEl.innerText = "You are logged in."
+                // if getRealTimeUpdates is started prior to someone being logged in errors occur due to the current Firestore permissions I have set.
+                getRealTimeUpdates()
             } else {
-                // console.log("not logged in")
+                // User is not logged in
                 loginStatusEl.innerText = "You are NOT logged in."               
             }
         })
@@ -62,14 +64,18 @@ function main(){
     authenticationState()
 
     function voteBtn(){
+        // Start event listener for vote button
         voteBtnEl.addEventListener("click", function(){
+            // call getVote to get the users vote
             const vote = getVote();
+            // call saveToDatabase to save the users vote to the database
             saveToDatabase(vote);
         })
     }
     voteBtn()
 
     function getVote(){
+        // loops through all the choice elements and sees if one is checked. If checked, that checked element is the vote.
         for(let i=0; i < petChoiceEls.length; i++){
             if(petChoiceEls[i].checked){
                 return petChoiceEls[i].value;
@@ -81,25 +87,28 @@ function main(){
         const textToSave = vote;
         console.log(`I am going to save ${vote} as the best pet.`)
         try{ 
-            const response = await docRef.set({
+            // sends the users vote to the Firestore database
+            await docRef.set({
                 petStatus: textToSave
             })
-            console.log("Pet vote saved!: ", response)
+            console.log("Pet vote saved!")
         }
         catch(err){
             console.log("Error occurred trying to save vote: ", err)
         } 
     }
-
+    
+    // continuely listens for updates to the database at the specific database document "docRef"
     function getRealTimeUpdates(){
+        // starts the listener
         docRef.onSnapshot(function(doc){
             if(doc && doc.exists) {
+                // gets the data from the database when an update occurs
                 const myData = doc.data();
+                // sets the pet display el to the updated value of petStatus
                 petDislayEl.innerHTML = myData.petStatus;
             }
         })
     }
-    getRealTimeUpdates()
-
 }
 main();
